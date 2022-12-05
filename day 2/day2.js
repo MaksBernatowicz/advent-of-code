0c544bc60355cac1441e7cb2bba8711b5f4fa2b0
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-12-05 11:17:48 +0100

    day2

---
+++ day 2/day2.js
@@ -0,0 +1,23 @@
+// 👇️ if using ES6 Imports uncomment line below
+// import { readFileSync } from "fs";
+
+const { readFileSync } = require("fs");
+
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, "utf-8");
+  const arr = contents.split(/\r?\n/);
+
+  return arr;
+}
+data = syncReadFile("./sample.txt");
+console.log(`Input data: ${data}`);
+console.log(`array length: ${data.length}`);
+console.log(
+  "********************************************************************************************************"
+);
+
+// const program = () => {
+
+// };
+
+// program(data);
